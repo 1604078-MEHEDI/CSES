1802e6e5e9cce03ba0f3e9a72fa16b97bceb0462
Mehedi Hasan <31961808+1604078-MEHEDI@users.noreply.github.com>
2019-10-01 12:16:28 +0600

    Create 1092.cpp

---
+++ 1092.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1<<30
+#define endl '\n'
+#define maxn 100005
+#define tc printf("Case %d: ", cs)
+#define tcn printf("Case %d:\n", cs);
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+const double PI = acos(-1.0);
+
+#define dbg1(x) cerr << #x << " = " << x << endl;
+#define dbg2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl;
+#define dbg3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
+#define dbg4(w,x, y, z) cerr << #w << " = " << w << ", " <<#x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        if ( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        if ( it != v.begin()) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        if ( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+clock_t tStart = clock();
+#define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+
+void faltu () { cerr << endl; }
+
+template <typename T>
+void faltu( T a[], int n ) {
+    for (int i = 0; i < n; ++i) cerr << a[i] << ' ';
+    cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) { cerr << arg << ' '; faltu(rest...); }
+// Program showing a policy-based data structure.
+#include <ext/pb_ds/assoc_container.hpp> // Common file 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less 
+#include <iostream>
+using namespace __gnu_pbds;
+using namespace std;
+// GNU link : https://goo.gl/WVDL6g
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+
+/**___________________________________________________**/
+
+
+map<int, int> mp;
+int main()
+{
+    FASTIO
+    /*
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+    #endif
+    //*/
+    int T;
+    //scanf("%d", &T);
+    T = 1;
+    for (int cs = 1; cs <= T; cs++) {
+        ll n;
+        cin >> n;
+        ll m = n * (n + 1) / 2;
+        if (m & 1) {
+            cout << "NO\n";
+            return 0;
+        }
+        else {
+            m /= 2ll;
+            cout << "YES\n";
+            set<int> l;
+            l.insert(n);
+            mp[n] = 1;
+            m -= n;
+            // dbg(m);
+            ll cnt = n - 1;
+            while (m > 0) {
+                if (m <= 0) break;
+                if (m < cnt) {
+                    l.insert(m);
+                    mp[m] = 1;
+                    break;
+                }
+                l.insert(cnt);
+                m -= cnt;
+                mp[cnt] = 1;
+                cnt--;
+            }
+            cout << (int)l.size() << endl;
+            for (auto x : l) cout << x << " ";
+            cout << endl;
+            cout << n - (int)l.size() << endl;
+            for (int i = 1; i <= n; i++) {
+                if (mp[i]) continue;
+                cout << i << " ";
+            }
+        }
+    }
+    return 0;
+}
